1c387c58b2a69972747a945c3a55cfcc27de4841
김창민 <changmin@gimchangmin-ui-MacBookPro.local>
2025-08-04 21:53:14 +0900

    lab: mvc-gateway 추가

---
+++ gateway-mvc/src/main/kotlin/study/msa/gatewaymvc/GatewayMvcApplication.kt
@@ -0,0 +1,11 @@
+package study.msa.gatewaymvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GatewayMvcApplication
+
+fun main(args: Array<String>) {
+    runApplication<GatewayMvcApplication>(*args)
+}
---
+++ gateway-mvc/src/test/kotlin/study/msa/gatewaymvc/GatewayMvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package study.msa.gatewaymvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GatewayMvcApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
